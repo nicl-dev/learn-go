6dc4f3ea503601631c47fd629c8df94fc2e9ea55
niclas.behrendt <niclas.behrendt@tallence.com>
2024-04-02 11:34:35 +0200

    moved bank main function to a switch statement

--- bank/main.go
+++ bank/main.go
@@ -4,47 +4,54 @@ import "fmt"
 
 func main() {
 	var accountBalance float64 = 1000
+	var choice int
 
-	for i := 0; i < 2; i++ {
-		fmt.Println("Welcome to the Bank!")
+	fmt.Println("Welcome to the Bank!")
+
+	for {
 		fmt.Println("Please select an option:")
 		fmt.Println("1. Check balance")
 		fmt.Println("2. Deposit money")
 		fmt.Println("3. Withdraw money")
 		fmt.Println("4. Exit")
-
 		fmt.Print("Enter your choice: ")
-		var choice int
 		fmt.Scan(&choice)
 
-		if choice == 1 {
-			fmt.Println("Your balance is: ", accountBalance)
-		} else if choice == 2 {
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is:", accountBalance)
+		case 2:
 			var depositAmount float64
 			fmt.Print("Enter the amount you want to deposit: ")
 			fmt.Scan(&depositAmount)
 
 			if depositAmount <= 0 {
 				fmt.Println("Invalid deposit amount. Must be greater than 0.")
-				return
+				continue
 			}
 
 			accountBalance += depositAmount
-			fmt.Println("Your new balance is: ", accountBalance)
-		} else if choice == 3 {
+			fmt.Println("Your new balance is:", accountBalance)
+		case 3:
 			var withdrawAmount float64
 			fmt.Print("Enter the amount you want to withdraw: ")
 			fmt.Scan(&withdrawAmount)
 
-			if withdrawAmount >= accountBalance {
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid deposit amount. Must be greater than 0.")
+				continue
+			}
+
+			if withdrawAmount > accountBalance {
 				fmt.Println("Invalid deposit amount. Not enough funds in account.")
-				return
+				continue
 			}
 
 			accountBalance -= withdrawAmount
 			fmt.Println("Your new balance is: ", accountBalance)
-		} else {
-			fmt.Println("Goodbye!")
+		default:
+			fmt.Println("Thank you for choosing our Bank!")
+			return
 		}
 	}
 }
