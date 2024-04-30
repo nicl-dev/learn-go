b5684a72a5950e531e32153008c97329098085dc
niclas.behrendt <niclas.behrendt@tallence.com>
2024-04-30 12:46:51 +0200

    created function getNoteData for the note data

--- practice-projects/structs-practice/main.go
+++ practice-projects/structs-practice/main.go
@@ -6,22 +6,12 @@ import (
 )
 
 func main() {
-	title, err := getUserInput("Note title:")
+	title, content, err := getNoteData()
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	content, err := getUserInput("Note content:")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(title)
-	fmt.Println(content)
 }
 
 func getUserInput(prompt string) (string, error) {
@@ -32,4 +22,20 @@ func getUserInput(prompt string) (string, error) {
 		return "", errors.New("invalid input")
 	}
 	return value, nil
+}
+
+func getNoteData() (string, string, error) {
+	title, err := getUserInput("Note title:")
+
+	if err != nil {
+		return "", "", err
+	}
+
+	content, err := getUserInput("Note content:")
+
+	if err != nil {
+		return "", "", err
+	}
+
+	return title, content, nil
 }
