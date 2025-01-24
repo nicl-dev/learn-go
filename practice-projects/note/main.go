51500a89077a58fcce66932e178989f1f0bfb6b1
behren <mobile.niclas@gmail.com>
2025-01-24 21:53:46 +0100

    created first helper function and added it to main

--- practice-projects/note/main.go
+++ practice-projects/note/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	_, err := getUserInput("Enter your note.")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func getUserInput(prompt string) (string, error) {
+	fmt.Println(prompt)
+
+	var value string
+	_, err := fmt.Scanln(&value)
+	if err != nil {
+		return "", errors.New("Invalid input.")
+	}
+
+	return value, nil
+}
