0a9c852b4b8917d1c0eb515e45cae74ed565be61
behren <mobile.niclas@gmail.com>
2025-01-26 07:57:46 +0100

    init new todo package

---
+++ practice-projects/note/todo/todo.go
@@ -0,0 +1,41 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Todo struct {
+	Text string `json:"text"`
+}
+
+func (t Todo) Display() {
+	fmt.Println(t)
+}
+
+func (t Todo) Save() error {
+	fileName := "todo.json"
+
+	json, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(fileName, json, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func New(todoContent string) (Todo, error) {
+	if todoContent == "" {
+		return Todo{}, errors.New("Invalid input.")
+	}
+
+	return Todo{
+		Text: todoContent,
+	}, nil
+}
