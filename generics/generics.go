901212ad360a483300ea456b98833fd39ad4ad65
behren <mobile.niclas@gmail.com>
2025-01-30 17:38:24 +0100

    finished add function to test generics

--- generics/generics.go
+++ generics/generics.go
@@ -1,9 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	result := add(1, 2)
+	fmt.Println(result)
+
+	secondResult := add("hello ", "sir")
+	fmt.Println(secondResult)
 }
 
-func add(a, b any) any {
-
+func add[T int | float64 | string](a, b T) T {
+	return a + b
 }
