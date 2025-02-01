73336e45fc5d13aa4b322b57f457d8e6819a6b84
niclas.behrendt <niclas.behrendt@tallence.com>
2025-02-01 20:45:28 +0100

    added slices and tested them

--- arrays-slices-maps/lists.go
+++ arrays-slices-maps/lists.go
@@ -6,7 +6,15 @@ func main() {
 	var productNames [4]string = [4]string{"A book"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
+	fmt.Println(prices[3])
+
+	fmt.Println(productNames)
+	productNames[3] = "A carpet"
 	fmt.Println(productNames)
 
-	fmt.Println(prices[3])
+	featuredPrices := prices[1:]
+	featuredPrices[0] = 199.99
+	highlitedPrices := featuredPrices[:1]
+	fmt.Println(featuredPrices)
+	fmt.Println(highlitedPrices)
 }
