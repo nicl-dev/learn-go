07830ccb0615cd6dff8d3118e30ce8e94406ee2d
niclas.behrendt <niclas.behrendt@tallence.com>
2025-02-01 21:09:07 +0100

    added new function to test slices

--- arrays-slices-maps/lists.go
+++ arrays-slices-maps/lists.go
@@ -3,23 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	var productNames [4]string = [4]string{"A book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	prices := []float64{10.99, 8.99}
+
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
+
+	prices = append(prices, 14.99)
 	fmt.Println(prices)
-	fmt.Println(prices[3])
-
-	fmt.Println(productNames)
-	productNames[3] = "A carpet"
-	fmt.Println(productNames)
-
-	featuredPrices := prices[1:]
-	featuredPrices[0] = 199.99
-	highlitedPrices := featuredPrices[:1]
-	fmt.Println(featuredPrices)
-	fmt.Println(highlitedPrices)
-	fmt.Println(len(highlitedPrices), cap(highlitedPrices))
-
-	highlitedPrices = highlitedPrices[:3]
-	fmt.Println(highlitedPrices)
-	fmt.Println(len(highlitedPrices), cap(highlitedPrices))
 }
+
+// below: function to test creating and modifying arrays
+
+// func main() {
+// 	var productNames [4]string = [4]string{"A book"}
+// 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+// 	fmt.Println(prices)
+// 	fmt.Println(prices[3])
+
+// 	fmt.Println(productNames)
+// 	productNames[3] = "A carpet"
+// 	fmt.Println(productNames)
+
+// 	featuredPrices := prices[1:]
+// 	featuredPrices[0] = 199.99
+// 	highlitedPrices := featuredPrices[:1]
+// 	fmt.Println(featuredPrices)
+// 	fmt.Println(highlitedPrices)
+// 	fmt.Println(len(highlitedPrices), cap(highlitedPrices))
+
+// 	highlitedPrices = highlitedPrices[:3]
+// 	fmt.Println(highlitedPrices)
+// 	fmt.Println(len(highlitedPrices), cap(highlitedPrices))
+// }
