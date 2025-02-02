0a157f0503ab7d7fb9afd1989812c2bb54718ac3
behren <mobile.niclas@gmail.com>
2025-02-02 19:30:26 +0100

    set up basic map

--- arrays-slices-maps/maps.go
+++ arrays-slices-maps/maps.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	websites := map[string]string{
+		"Google":              "https://google.com",
+		"Amazon Web Services": "https://aws.com",
+	}
+	fmt.Println(websites)
 }
