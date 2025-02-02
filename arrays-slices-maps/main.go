fb393db7399abf35b6724d205434017d0cbe2e7d
behren <mobile.niclas@gmail.com>
2025-02-02 19:56:13 +0100

    created a map with make

--- arrays-slices-maps/main.go
+++ arrays-slices-maps/main.go
@@ -12,4 +12,12 @@ func main() {
 	userNames = append(userNames, "Manuel")
 
 	fmt.Println(userNames)
+
+	courseRatings := make(map[string]float64, 3)
+
+	courseRatings["go"] = 4.7
+	courseRatings["react"] = 4.8
+	courseRatings["angular"] = 4.7
+	courseRatings["vue"] = 4.9
+	fmt.Println(courseRatings)
 }
