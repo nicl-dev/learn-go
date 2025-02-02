3d3285d1de1d369d75d898ee1663d073b3c7a483
behren <mobile.niclas@gmail.com>
2025-02-02 20:00:04 +0100

    added custom type for float map

--- arrays-slices-maps/main.go
+++ arrays-slices-maps/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (f floatMap) output() {
+	fmt.Println(f)
+}
+
 func main() {
 	userNames := make([]string, 2, 5)
 	fmt.Println(userNames)
@@ -13,11 +19,12 @@ func main() {
 
 	fmt.Println(userNames)
 
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 4.8
 	courseRatings["angular"] = 4.7
 	courseRatings["vue"] = 4.9
-	fmt.Println(courseRatings)
+
+	courseRatings.output()
 }
