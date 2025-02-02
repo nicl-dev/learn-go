6be161cbf471e36eb80dfd7aa3c5e5ff87714f0f
behren <mobile.niclas@gmail.com>
2025-02-02 19:52:02 +0100

    added capacity to make call

--- arrays-slices-maps/main.go
+++ arrays-slices-maps/main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	userNames := make([]string, 2)
+	userNames := make([]string, 2, 5)
 	fmt.Println(userNames)
 
 	userNames[0] = "Julie"
+
+	userNames = append(userNames, "Max")
+	userNames = append(userNames, "Manuel")
 
 	fmt.Println(userNames)
 }
