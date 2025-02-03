a9a0bf02c01d9cc2a5cc2bc04c62b8fa967d8d2a
behren <mobile.niclas@gmail.com>
2025-02-03 17:27:17 +0100

    moved functions to a new package to test anonymous functions

---
+++ functions-deep-dive/functionsarevalues/main.go
@@ -0,0 +1,48 @@
+package functionsarevalues
+
+import "fmt"
+
+type transformFn func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 6, 7, 8}
+	doubled := multiplyNumbers(&numbers, double)
+	fmt.Println(doubled)
+	tripled := multiplyNumbers(&numbers, triple)
+	fmt.Println(tripled)
+
+	transformFn1 := getTransfomerFunction(&numbers)
+	transformFn2 := getTransfomerFunction(&moreNumbers)
+
+	transformedNumbers := multiplyNumbers(&numbers, transformFn1)
+	moreTransformedNumbers := multiplyNumbers(&moreNumbers, transformFn2)
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
+}
+
+func multiplyNumbers(numbers *[]int, transform transformFn) []int {
+	dNumbers := []int{}
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+
+	return dNumbers
+}
+
+func getTransfomerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
--- functions-deep-dive/main.go
+++ functions-deep-dive/main.go
@@ -2,26 +2,15 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
-
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 6, 7, 8}
-	doubled := multiplyNumbers(&numbers, double)
-	fmt.Println(doubled)
-	tripled := multiplyNumbers(&numbers, triple)
-	fmt.Println(tripled)
+	numbers := []int{1, 2, 3}
 
-	transformFn1 := getTransfomerFunction(&numbers)
-	transformFn2 := getTransfomerFunction(&moreNumbers)
+	transformed := transformNumbers(&numbers, ???)
 
-	transformedNumbers := multiplyNumbers(&numbers, transformFn1)
-	moreTransformedNumbers := multiplyNumbers(&moreNumbers, transformFn2)
-	fmt.Println(transformedNumbers)
-	fmt.Println(moreTransformedNumbers)
+	fmt.Println(transformed)
 }
 
-func multiplyNumbers(numbers *[]int, transform transformFn) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
 
 	for _, val := range *numbers {
@@ -29,20 +18,4 @@ func multiplyNumbers(numbers *[]int, transform transformFn) []int {
 	}
 
 	return dNumbers
-}
-
-func getTransfomerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return triple
-	}
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
