92152b4a7209fd4122b12282415c573f3b5d96f8
behren <mobile.niclas@gmail.com>
2025-02-03 19:15:22 +0100

    added first variadic function

--- functions-deep-dive/main.go
+++ functions-deep-dive/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	sum := sumup(1, 2, 3, 4)
+
+	fmt.Println(sum)
+}
+
+func sumup(numbers ...int) int {
+	sum := 0
+
+	for _, i := range numbers {
+		sum += i
+	}
+
+	return sum
 }
