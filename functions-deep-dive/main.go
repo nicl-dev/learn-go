e86da5a449e3aba1598c0b8e6ef1c10aaa274df7
behren <mobile.niclas@gmail.com>
2025-02-03 16:55:16 +0100

    created function that needs another function as paremeter

--- functions-deep-dive/main.go
+++ functions-deep-dive/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	numbers := []int{2, 4, 6}
+	doubled := multiplyNumbers(&numbers, double)
+	fmt.Println(doubled)
+	tripled := multiplyNumbers(&numbers, triple)
+	fmt.Println(tripled)
+}
+
+func multiplyNumbers(numbers *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+
+	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }
