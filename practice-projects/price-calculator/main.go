a963748964cb9adc5ca05d8d6ef3b2fbfc544e13
behren <mobile.niclas@gmail.com>
2025-02-05 17:12:40 +0100

    added first tax calculations

--- practice-projects/price-calculator/main.go
+++ practice-projects/price-calculator/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.07, 0.10, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, price := range prices {
+		for _, taxRate := range taxRates {
+			result[taxRate] = append(result[taxRate], price*(1+taxRate))
+		}
+	}
+
+	fmt.Println(result)
 }
