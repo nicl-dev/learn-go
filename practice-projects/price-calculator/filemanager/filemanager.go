396797980642a6feef6e51ae441c240ce57298b2
behren <mobile.niclas@gmail.com>
2025-02-13 22:40:47 +0100

    prepared new filemanager struct and constructor

--- practice-projects/price-calculator/filemanager/filemanager.go
+++ practice-projects/price-calculator/filemanager/filemanager.go
@@ -7,8 +7,13 @@ import (
 	"os"
 )
 
-func ReadLines(path string) ([]string, error) {
-	file, err := os.Open(path)
+type FileManager struct {
+	InputFilePath  string
+	OutputFilePath string
+}
+
+func (f *FileManager) ReadLines() ([]string, error) {
+	file, err := os.Open(f.InputFilePath)
 
 	if err != nil {
 		return nil, errors.New("failed to open file")
@@ -31,8 +36,8 @@ func ReadLines(path string) ([]string, error) {
 	return lines, nil
 }
 
-func WriteJSON(path string, data any) error {
-	file, err := os.Create(path)
+func (f *FileManager) WriteJSON(data any) error {
+	file, err := os.Create(f.OutputFilePath)
 
 	if err != nil {
 		return errors.New("failed to create file")
@@ -48,4 +53,16 @@ func WriteJSON(path string, data any) error {
 
 	file.Close()
 	return nil
+}
+
+func New(InputFilePath, OutputFilePath string) (FileManager, error) {
+	if InputFilePath == "" || OutputFilePath == "" {
+		err := errors.New("missing input or output file path")
+		return FileManager{}, err
+	}
+
+	return FileManager{
+		InputFilePath:  InputFilePath,
+		OutputFilePath: OutputFilePath,
+	}, nil
 }
